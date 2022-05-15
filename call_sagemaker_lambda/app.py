5777b26c7b154b900bcae3ce1306bcf9414f7a76
ManuLasker <emanuelule2828@gmail.com>
2022-05-15 00:43:18 -0500

    feat: add a method that clean s3 files after inference

--- call_sagemaker_lambda/app.py
+++ call_sagemaker_lambda/app.py
@@ -5,35 +5,39 @@ import uuid
 import os
 from PIL import Image
 from src.utils.image_utilities import pil_to_base64 
-from src.utils.aws_utilities import s3_check_to_download, s3_upload_file
+from src.utils.aws_utilities import (s3_check_to_download,
+                                     s3_upload_file,
+                                     s3_clean_files)
 
 sg = boto3.client("sagemaker-runtime", region_name="us-east-1")
 
 def handler(event, context = None):
     with Image.open(event.get("input_image_path", "input/carta_laboral_image.jpg")) as pil_image:
         base64_im = pil_to_base64(pil_image)
+        # create input file
         identifier = str(uuid.uuid4())
         bucket_name = event["bucket_result_name"]
         key = f'{identifier}.json'
+        # Upload input model file
         s3_upload_file(base64_im, bucket_name, key)
+        # Make async prediction
         response = sg.invoke_endpoint_async(
             EndpointName = event["endpoint_name"],
             InputLocation = f's3://{bucket_name}/{key}',
             InferenceId = identifier,
             ContentType = "application/json"
         )
-        print(json.dumps(response))
+        # Download prediction
         endpoint_result = s3_check_to_download(response["OutputLocation"])
+        # Clean input and output files
+        s3_clean_files(f's3://{bucket_name}/{key}', response["OutputLocation"])
         return endpoint_result
-
-def clean():
-    os.system("rm *.json")
 
 if __name__ == "__main__":
     print("Make a call to a sagemaker async endpoint")
-    handler({
+    result = handler({
         "endpoint_name": "nu0087002ei-aid-dev-firma-model",
         "bucket_result_name": "nu0087002ei-aid-dev-inference-results-bucket",
         "input_image_path": sys.argv[1]
     })
-    clean()
+    print(result)
--- call_sagemaker_lambda/src/utils/aws_utilities.py
+++ call_sagemaker_lambda/src/utils/aws_utilities.py
@@ -34,13 +34,41 @@ def s3_check_to_download(s3_result_path: str):
         try:
             with io.BytesIO() as f:
                 s3.download_fileobj(bucket, key, f)
-                return json.loads(f.read().decode())
+                return json.loads(f.getvalue().decode())
         except botocore.exceptions.ClientError as error:
-            if error.response['Error']['Code'] == 'NoSuchKey':
-                print('waiting for output to be processed')
+            if error.response['Error']['Code'] in ['NoSuchKey', '404']:
+                print(f'waiting for output to be processed, output_location: {s3_result_path}')
                 time.sleep(2)
                 continue
             raise Exception(f'There was an error searching for results in bucket: {bucket}'
                             f', error: {error}'
                             f', key: {key}')
     raise Exception(f'Timeout Exception, waiting time excedded, time limit {time_limit}')
+
+def s3_clean_files(*s3_paths, version_enabled: bool = True):
+    """clean all files on s3 bucket where file are stored
+    """
+    for s3_path in s3_paths:
+        s3_path_url = urllib.parse.urlparse(s3_path)
+        bucket = s3_path_url.netloc
+        key = s3_path_url.path[1:]
+        versions = None
+        if version_enabled:
+            versions = s3.list_object_versions(
+                Bucket = bucket,
+                Prefix = key
+            )['Versions']
+            versions = [version['VersionId'] for version in versions]
+            for version_id in versions:
+                print(f'deleting object: {key} with version {version_id}')
+                s3.delete_object(
+                    Bucket = bucket,
+                    Key = key,
+                    VersionId = version_id
+                )
+        else:
+            print(f'deleting object: {key}')
+            s3.delete_object(
+                Bucket = bucket,
+                Key = key
+            )
