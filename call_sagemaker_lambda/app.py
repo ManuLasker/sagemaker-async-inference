fe5103a0748c44a5bc8400fa3a2e3acd45b83707
ManuLasker <emanuelule2828@gmail.com>
2022-05-14 22:30:39 -0500

    refactor: add new files to .gitignore

--- call_sagemaker_lambda/app.py
+++ call_sagemaker_lambda/app.py
@@ -4,39 +4,36 @@ import boto3
 import uuid
 import os
 from PIL import Image
-from utils.image_utilities import *
-from utils.aws_utilities import *
+from src.utils.image_utilities import pil_to_base64 
+from src.utils.aws_utilities import s3_check_to_download, s3_upload_file
 
 sg = boto3.client("sagemaker-runtime", region_name="us-east-1")
 
 def handler(event, context = None):
-    with Image.open(event["input_image_path"]) as pil_image:
+    with Image.open(event.get("input_image_path", "input/carta_laboral_image.jpg")) as pil_image:
         base64_im = pil_to_base64(pil_image)
         identifier = str(uuid.uuid4())
-        input_location = f's3://{event["bucket_result_name"]}/{identifier}.json'
-        with open(f"{identifier}.json", "w") as payload_file:
-            json.dump(base64_im, payload_file)
-            command = f'aws s3 cp ./{identifier}.json {input_location}'
-            print(command)
-            os.system(command)
+        bucket_name = event["bucket_result_name"]
+        key = f'{identifier}.json'
+        s3_upload_file(base64_im, bucket_name, key)
         response = sg.invoke_endpoint_async(
             EndpointName = event["endpoint_name"],
-            InputLocation = input_location,
+            InputLocation = f's3://{bucket_name}/{key}',
             InferenceId = identifier,
             ContentType = "application/json"
         )
-        print(response)
-        print(json.dumps(response, indent=2))
+        print(json.dumps(response))
+        endpoint_result = s3_check_to_download(response["OutputLocation"])
+        return endpoint_result
 
 def clean():
     os.system("rm *.json")
 
 if __name__ == "__main__":
     print("Make a call to a sagemaker async endpoint")
-    for i in range(20):
-        handler({
-            "endpoint_name": "nu0087002ei-aid-dev-firma-model",
-            "bucket_result_name": "nu0087002ei-aid-dev-inference-results-bucket",
-            "input_image_path": sys.argv[1]
-        })
+    handler({
+        "endpoint_name": "nu0087002ei-aid-dev-firma-model",
+        "bucket_result_name": "nu0087002ei-aid-dev-inference-results-bucket",
+        "input_image_path": sys.argv[1]
+    })
     clean()
