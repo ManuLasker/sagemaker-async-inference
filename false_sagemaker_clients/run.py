6a1952298bd295f57e710e5e7491e2667267b0d9
manulasker <emanuelule2828@gmail.com>
2022-05-23 00:55:54 -0500

    feat (cli app): complete cli app to call lambda function, recreating N clients making request to the lambda in an asynchronous way and generating a csv report

--- false_sagemaker_clients/run.py
+++ false_sagemaker_clients/run.py
@@ -1,26 +1,43 @@
-from typing import Optional
 from pathlib import Path
-from src.callbacks import validate_file_callback
+from src.callbacks import validate_file_callback, validate_create_directory_callback
+from src.lambda_controller import main as lambda_main
 
 import typer
-import asyncio
 
 app = typer.Typer()
 
 @app.command()
 def call_lambda(
-    lambda_name: str = typer.Argument(..., help="Name of the lambda to execute asynchronously"),
-    false_clients_number: int = typer.Option(..., "-n", help="Number of false clients to create"),
-    lambda_event_file_path: Path = typer.Option(..., "--event-file", help="Event json file to send")
+    lambda_name: str = typer.Argument("nu0087002ei-aid-dev-sagemaker-async-lambda",
+                                      help="Name of the lambda to execute asynchronously"),
+    false_clients_number: int = typer.Option(10, "-n",
+                                             help="Number of false clients to create"),
+    lambda_event_file_path: Path = typer.Option("inputs/lambda_event.json",
+                                                "--event-file",
+                                                help="Event json file to send",
+                                                callback=validate_file_callback),
+    result_directory_path: Path = typer.Option("results", 
+                                               "--result-path",
+                                               help="Results directory where to save"
+                                               " the metadata for our test",
+                                               callback=validate_create_directory_callback)
 ):
-    pass
+    lambda_main(false_clients_number, lambda_name,
+                lambda_event_file_path, result_directory_path)
 
 @app.command()
 def call_sagemaker(
-    sagemaker_endpoint_name: str = typer.Argument(..., help="Name of the sagemaker endpoint to execute asynchronously"),
-    false_clients_number: int = typer.Option(..., "-n", help="Number of false clients to create"),
-    image_file_path: Path = typer.Option(..., "--image-file", help="Image input file to send", callback=validate_file_callback)
+    sagemaker_endpoint_name: str = typer.Argument(..., 
+                                                  help="Name of the sagemaker"
+                                                  " endpoint to execute asynchronously"),
+    false_clients_number: int = typer.Option(...,
+                                             "-n",
+                                             help="Number of false clients to create"),
+    image_file_path: Path = typer.Option(..., "--image-file",
+                                         help="Image input file to send", 
+                                         callback=validate_file_callback)
 ):
+    print("not implemented yet!")
     pass
 
 if __name__ == "__main__":
